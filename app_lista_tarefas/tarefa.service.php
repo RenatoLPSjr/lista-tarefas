e218bb92ee234af5c38b4373199af181d58484ea
Renato Lopes <renato.lop.jr@gmail.com>
2023-02-27 18:32:04 -0300

    Arrumando o banco de daods

---
+++ app_lista_tarefas/tarefa.service.php
@@ -0,0 +1,38 @@
+<?php
+
+class TarefaService
+{
+    private $conexao;
+    private $tarefa;
+
+    public function __construct(Conexao $conexao, Tarefa $tarefa){
+        $this->conexao = $conexao->conectar();
+        $this->tarefa = $tarefa;
+    }
+
+    public function inserir()
+    {
+        $query = 'insert into tb_tarefas(tarefa)values(:tarefa)';
+        $stmt = $this->conexao->prepare($query);
+        $stmt->bindValue(':tarefa', $this->tarefa->__get('tarefa'));
+        $stmt->execute();
+    }
+
+    public function recuperar()
+    {
+        $query = 'select id, id_status, tarefas from tb_tarefas';
+        $stmt = $this->conexao->prepare($query);
+        $stmt->execute();
+        return $stmt->fetchAll(PDO::FETCH_OBJ);
+    }
+
+    public function atualizar()
+    {
+
+    }
+
+    public function remover()
+    {
+
+    }
+}
